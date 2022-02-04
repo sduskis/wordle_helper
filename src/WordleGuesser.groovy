7af43aa1d1363a8efa5e407e9406eb0aa67cbc80
sduskis <sduskis@gmail.com>
2022-02-04 15:32:36 -0500

    Adding more comments, further first word filter

--- src/WordleGuesser.groovy
+++ src/WordleGuesser.groovy
@@ -1,40 +1,46 @@
+def notFound = /.*/ // Replace with "/.*[some letters].*/" once you find disallowed letters
 
-def disallowed = /.*[rismouh].*/
-def green = /^..ea.$/
-def yellow = ["a": [1], "t": [0,3], "e": [1]]
+// Replace with something like /^...a.e$/ if a is required in the third position
+// and e is required in the last position.
+def green = /^.....$/
 
-def isGood = {
-    word ->
-        !(word ==~ disallowed) && (word ==~ green)
-                && yellow.entrySet().every(entry ->
-                word.contains(entry.key)
-                        && !entry.value.any { id -> word[id] == entry.key })
-}
+// Replace with something like ["a": [1], "t": [0,3], "e": [1]] if a, t, e are
+// required but not in those positions.  Note, these numbers are 0 based.
+// In this example, "a" cannot be in position 2 (i.e. /.a..../), and "t"
+// cannot be in position 1 or 4 (i.e. /t..../ nor /...t./)
+def yellow = [:]
 
-def uniqueLetters = {
-    (String word) ->
-            new HashSet<>(word.toList()).size() == word.size();
-}
+def words = new File("wordle_words.txt").readLines()
 
-def wordFile = new File("wordle_words.txt")
+if (notFound == ".*" && yellow.isEmpty()) {
+    // generates the initial guess... or just use "arise"
 
-if (disallowed.isEmpty()) {
-    def words = []
-    wordFile.eachLine(word -> {
-        if (uniqueLetters(word))
-            words.add(word)
-    })
+    // flnd all words where each letter is unique
+    words = words.findAll { word -> new HashSet<>(word.toList()).size() == word.length() }
+
+    // The first guess should have 3 unique vowels.
+    words = words.findAll(word -> word.toList().findAll { c -> "aeiou".contains(c) }.size() == 3)
 
     println "Word Count: ${words.size()}"
     def randomWordId = new java.util.Random().nextInt(words.size());
     println "Random Word: ${words[randomWordId]}"
 } else {
-    int size = 0
-    wordFile.eachLine(word -> {
-        if (isGood(word)) {
-            size++
-            println word
-        }
-    })
-    println size
+    def matchesYellow = {
+        word ->
+            yellow.entrySet().every(entry ->
+                    // the word contains the yellow letter
+                    word.contains(entry.key)
+                            // just not in the positions it was previously found
+                            && !entry.value.any { id -> word[id] == entry.key })
+    }
+
+    // find all words that:
+    //
+    // 1) don't have any letters in the "not found" list
+    // 2) match the exact positions of green letters
+    // 3) have yellow letters, just not in the position they were previously found
+
+    words = words.findAll { word -> !(word ==~ notFound) && (word ==~ green) && matchesYellow(word) }
+    words.forEach(System.out::println)
+    println "Matching word count: ${words.size()}"
 }
